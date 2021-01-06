e1cafa8c0759f2a2a7dc9609bf0e69ded096eae2
Tyrone Moore <mooret025@gmail.com>
2021-01-06 17:24:31 +0000

    Looping and stuff

---
+++ aForLoop.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+brush = ['g', 'g', 'b', 3, 9, 'g', 'dynamite']
+
+for item in brush:
+    if item == 'g':
+        print('SCREAM')
+    elif item == 'dynamite':
+        print("BOOM SHAKA LAKA!")
+
